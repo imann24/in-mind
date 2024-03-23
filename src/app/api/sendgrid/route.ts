25c31a9989ebe413f22ba2cb9c8f62e0f9245397
Isaiah Mann <isaiahbmann@gmail.com>
2024-03-23 04:41:26 -0700

    Add OpenAI integration for inspirational msg

--- src/app/api/sendgrid/route.ts
+++ src/app/api/sendgrid/route.ts
@@ -1,7 +1,25 @@
 import SendGrid from '@sendgrid/mail'
 import { createClient, type SupabaseClient } from '@supabase/supabase-js'
+import OpenAI from 'openai'
 
 export const dynamic = 'force-dynamic'
+
+async function getOpenAIResponse(coreValues: string[]): Promise<string> {
+  if (!process.env.OPENAI_API_KEY) {
+    console.error('openai-api', 'OpenAI API key not set')
+    return 'Unable to contact ChatGPT'
+  }
+
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+  const answer = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-1106',
+    messages: [{
+      role: 'user',
+      content: `Please generate an inspirational message for me based on my core values: ${coreValues.join(', ')}`,
+    }],
+  })
+  return JSON.parse(answer.choices[0].message.content as string)
+}
 
 export async function GET(req: Request) {
   if (!process.env.SENDGRID_API_KEY ||
@@ -31,6 +49,7 @@ export async function GET(req: Request) {
     return Response.json({ error }, { status: 500 })
   }
 
+  const inspirationalMessage = await getOpenAIResponse(data[0].values)
   // TODO: switch this email from hardcoded to a list of emails from the database
   const response = await SendGrid.send({
     to: 'isaiahbmann@gmail.com',
@@ -42,6 +61,7 @@ export async function GET(req: Request) {
     dynamicTemplateData: {
       // TODO: pull these from Supabase:
       values: data[0].values,
+      inspirational_message: inspirationalMessage,
     },
   })
   return Response.json(response, { status: 200 })
